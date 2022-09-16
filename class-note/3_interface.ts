9649d1e52486667ad29da84533a09548aa2c7b5c
youahleum <dkfma6033@naver.com>
2022-09-16 15:30:14 +0900

    interface function

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,30 @@ interface User{
   name:string;
 }
 
+// 변수에 인터페이스 활용
 var seho :User={
   age:33,
   name:'세호'
+}
+
+// 함수에 인터페이스 활용
+function getUser(user:User){
+  console.log(user)
+}
+const capt={
+  name:"캡틴",
+  age:100
+}
+
+getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+
+interface SumFunction{
+  (a:number, b:number):number;
+}
+
+var sum:SumFunction;
+sum=function(a:number,b:number):number{
+  return a+b;
 }
