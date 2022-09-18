27c6102399854efcf20a2490cf8d89e0bf974f7c
youahleum <dkfma6033@naver.com>
2022-09-18 23:41:34 +0900

    generic

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -25,4 +25,37 @@ interface Dropdown<T>{
   selected:boolean;
 }
 
-const obj:Dropdown<number>={value:'abc', selected:true}
+const obj:Dropdown<string>={value:'abc', selected:true}
+
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text:T):T{
+//   console.log(text.length);
+//   return text;
+
+// }
+
+// logTextLength<string>('hi')
+
+function logTextLength<T>(text:T[]):T[]{
+  console.log(text.length);
+  text.forEach(function(text){
+    console.log(text)
+  })
+  return text;
+
+}
+
+// logTextLength<string>('hi')
+
+logTextLength<string>(['hi', 'abc'])
+
+// 제네릭 타입 제한2 - 정의된 타입 이용하기
+interface LengthType{
+  length:number;
+}
+
+function logTextLength2<T extends LengthType>(text:T):T{
+  text.length;
+  return text;
+}
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -15,7 +15,27 @@ const numberOfProducts :DropdownItem<number>[]= [
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item:DropdownItem<string> | DropdownItem<number>) {
+// function createDropdownItem(item:DropdownItem<string> | DropdownItem<number>) {
+//   const option = document.createElement('option');
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// // NOTE: 이메일 드롭 다운 아이템 추가
+// emails.forEach(function (email) {
+//   const item = createDropdownItem(email);
+//   const selectTag = document.querySelector('#email-dropdown');
+//   selectTag.appendChild(item);
+// });
+
+
+// 제네릭으로 받을 수 있는 타입을 .toString() 함수가 있는 타입으로 제한하는 것입니다. 
+// toString() API는 객체의 내장 API입니다. 따라서 해당 객체를 프로토 타입으로 받고 있는 
+// 문자, 숫자, 불린 등 주요 타입들에도 다 toString() API가 기본적으로 제공됩니다. 
+// 따라서 1번 코드 보다는 문자, 숫자 이외에 더 많은 타입을 수용할 수 있는 형태가 됩니다.
+function createDropdownItem<T extends {toString:Function}>(item:DropdownItem<T> ):HTMLOptionElement {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -25,7 +45,7 @@ function createDropdownItem(item:DropdownItem<string> | DropdownItem<number>) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
