5cdd3fdf19304a847384522c3dd1bc455290e3a7
youahleum <dkfma6033@naver.com>
2022-09-20 18:49:25 +0900

    타입추론 3

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -9,12 +9,30 @@ function getB(b=10){
 10+'10'// 1010
 
 // 타입 추론 기본 2
+// interface Dropdown2<T>{
+//   value:T;
+//   title:string;
+// }
+
+// var shoppingItem:Dropdown2<string>={
+//   value:'abc',
+//   title:'hello'
+// }
+
+// 타입 추론 기본 3
 interface Dropdown2<T>{
   value:T;
   title:string;
 }
+interface DetailedEropdown<K> extends Dropdown2<K>{
+  description:string;
+  tag:K;
+}
 
-var shoppingItem:Dropdown2<string>={
-  value:'abc',
-  title:'hello'
+var detailedItem:DetailedEropdown<number>={
+  title:'abc',
+  description:'ab',
+  value:'a',
+  tag:'a'
+
 }
