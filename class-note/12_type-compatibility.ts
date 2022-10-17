e1dc5325c7bd6176240b6708d8644118e472b7d8
youahleum <dkfma6033@naver.com>
2022-10-17 19:48:13 +0900

    타입 호환

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -1,1 +1,35 @@
-let
+// 타입 호환
+
+// 인터페이스
+interface Al{
+name:string;
+skill:string;
+}
+// interface Js{
+// name:string;
+// }
+
+class Js{
+  name:string;
+}
+
+var al: Al;
+var js : Js;
+
+
+// 작은 것에 큰 것을 넣을 수 있다.
+al= js;
+
+js= al;
+
+
+// 함수
+var add  = function(a:number){
+  //...
+}
+var sum =function (a:number, b:number){
+  //....
+}
+
+sum= add ; // 호환 가능 
+add = sum;  // 호환 불가능 , sum은 인자가 두개이고 add는 하나이기 때문
