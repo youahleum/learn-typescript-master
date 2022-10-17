3bd4dad51035b180340823ea6b02276be75d8052
youahleum <dkfma6033@naver.com>
2022-10-18 01:36:54 +0900

    타입호환- 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -23,7 +23,7 @@ al= js;
 js= al;
 
 
-// 함수
+// 함수  : 작은 범위의 함수안에 큰 범위으 함수를 할당하려고 하면 할당이 되지 않는다
 var add  = function(a:number){
   //...
 }
@@ -32,4 +32,24 @@ var sum =function (a:number, b:number){
 }
 
 sum= add ; // 호환 가능 
-add = sum;  // 호환 불가능 , sum은 인자가 두개이고 add는 하나이기 때문
+// add = sum;  // 호환 불가능 , sum은 인자가 두개이고 add는 하나이기 때문
+
+// 제네릭
+interface Empty<T>{
+  //..
+}
+
+var empty1:Empty<string>;
+var empty2:Empty<number>;
+
+empty1 = empty2; // 현재 interface Empty가 비어 있는 상황이기 때문에 둘다 호환이 된다 / 현재 빨간에러는 무시하고 보자
+empty2 = empty1;  
+
+interface NotEmpty<T>{
+  data:T;
+}
+var notempty1:NotEmpty<string>;
+var notempty2:NotEmpty<number>;
+
+notempty1 =notempty2;  // NotEmpty 안에 data가 들어있기 때문에 호환이 되지 않는다
+notempty2 =notempty1; 
