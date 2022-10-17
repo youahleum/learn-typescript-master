023e00245c45b58f004c781ca2a70754c5e66e8a
youahleum <dkfma6033@naver.com>
2022-10-18 02:27:32 +0900

    complete

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,14 +1,4 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
+import { Contact, PhoneType } from './types';
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
